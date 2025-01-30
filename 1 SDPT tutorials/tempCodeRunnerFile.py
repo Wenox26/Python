4d388e1e1902c2b41dbd69bd28baba2f5cbdd3b0
Owen <owenoxj@gmail.com>
2025-01-30 08:37:10 +0800

    Update 1.py

---
+++ 1 SDPT tutorials/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+firstNumber = int (input ("Enter First Number:"))
+secondNumber = int (input ("Enter Second Number:"))
+
+result = firstNumber / secondNumber
+
+print (str(firstNumber) + "/" + str(secondNumber) + "=" + str(result))
+
+print("Wow")
