efdc4b3b8d2a0068ab3673b6f64cfa6c0fe83cd3
Owen <owenoxj@gmail.com>
2025-03-28 12:35:11 +0800

    Update

--- 1 SDPT tutorials/12.py
+++ 1 SDPT tutorials/12.py
@@ -22,36 +22,39 @@
 # charTwo = Character()
 
 # Accessing attributes
-class Character:
-    name = "Name"
-    hp = 100
-    mp = 50
-    atk = 12
-    lvl = 1
-
-charOne = Character()
-charTwo = Character()
-
-charOne.name = "Wenox"
-charTwo.name = "Wenwen"
-print (charOne.name)
-print (charTwo.name)
-print (charOne.hp)
-
-# class Student:
+# class Character:
 #     name = "Name"
-#     age = 17
-#     yearlvl = "Grade 12"
-#     section = "HUMSS"
+#     hp = 100
+#     mp = 50
+#     atk = 12
+#     lvl = 1
 
-# studentOne = Student()
-# studentOne.name = "Owen"
-# studentOne.age = 18
+# charOne = Character()
+# charTwo = Character()
 
-# print(studentOne.name)
-# print(studentOne.age)
-# print(studentOne.yearlvl)
-# print(studentOne.section)
+# charOne.name = "Wenox"
+# charTwo.name = "Wenwen"
+# print (charOne.name)
+# print (charTwo.name)
+# print (charOne.hp)
+
+class Student:
+    name = "Name"
+    age = 17
+    yearlvl = "Grade 12"
+    section = "HUMSS"
+
+studentOne = Student()
+studentOne.name = "Owen"
+studentOne.age = 18
+studentOne.yearlvl= "Grade 1"
+
+print(studentOne.name)
+print(studentOne.age)
+print(studentOne.yearlvl)
+print(studentOne.section)
+
+
 
 # studentTwo = Student()
 # studentTwo.name = "Wenwen"
@@ -63,5 +66,3 @@ print (charOne.hp)
 # print(studentTwo.age)
 # print(studentTwo.yearlvl)
 # print(studentTwo.section)
-
-
---
+++ Practice/2.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    if y != 0:
+        return x / y
+    else:
+        print("Cannot be divided by zero")
+
+
+
+
+print("Python Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiplication")
+print("4. Division")
+
+
+print(" ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+
+
+
+
+# FUNCTION CALL
+# print(add(3,4))
+# print(sub(3,4))
+# print(mul(3,4))
+# print(div(3,4))
+
+
